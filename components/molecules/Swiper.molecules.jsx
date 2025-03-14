ab1af09514a8e2feecfd80f387f89f06ef65ce01
tkdking00 <musama4288921@gmail.com>
2025-03-15 01:23:58 +0500

    new upadates

--- components/molecules/Swiper.molecules.jsx
+++ components/molecules/Swiper.molecules.jsx
@@ -11,11 +11,10 @@ import { Pagination } from "swiper/modules";
 import SwiperCard from "./SwiperCard";
 
 export default function SwiperCumponent() {
-  
   return (
     <div className="">
       <Swiper
-      onResize={20}
+        onResize={20}
         slidesPerView={2}
         spaceBetween={30}
         pagination={{
@@ -26,8 +25,8 @@ export default function SwiperCumponent() {
         modules={[Pagination]}
         className="mySwiper "
         breakpoints={{
-          320: { slidesPerView: 1 },  // Mobile (1 slide)
-          768: { slidesPerView: 2 },  // Tablets & Desktops (2 slides max)
+          320: { slidesPerView: 1 }, // Mobile (1 slide)
+          768: { slidesPerView: 2 }, // Tablets & Desktops (2 slides max)
         }}
       >
         <SwiperSlide>
@@ -45,64 +44,49 @@ export default function SwiperCumponent() {
         <SwiperSlide>
           <SwiperCard
             srcl="/assets/images/bd3897c84c7024bf14ef51e08d8e0451.png"
-            srcr="/assets/images/client1.png"
-            text1="“Taylor is a professional
-                     Designer he really helps my
-                     business by providing value to
-                      my business."
-            text2="Tim Bailey"
-            text3="SEO Specialist, Theme Junction"
+            srcr="/assets/images/client2.png"
+            text1="“Working with Taylor was great. His designs added value and improved our online presence.”"
+            text2="Max"
+            text3="Marketing Head, Nova Trends"
           />
         </SwiperSlide>
         <SwiperSlide>
           <SwiperCard
             srcl="/assets/images/bd3897c84c7024bf14ef51e08d8e0451.png"
-            srcr="/assets/images/client1.png"
-            text1="“Taylor is a professional
-                     Designer he really helps my
-                     business by providing value to
-                      my business."
-            text2="Tim Bailey"
-            text3="SEO Specialist, Theme Junction"
+            srcr="/assets/images/client3.jpg"
+            text1="“Taylor's innovative designs made our brand stand out. His professionalism was impressive.”"
+            text2="Hamza Khan"
+            text3="Product Manager, StyleSphere"
           />
         </SwiperSlide>
         <SwiperSlide>
           <SwiperCard
             srcl="/assets/images/bd3897c84c7024bf14ef51e08d8e0451.png"
-            srcr="/assets/images/client1.png"
-            text1="“Taylor is a professional
-                     Designer he really helps my
-                     business by providing value to
-                      my business."
-            text2="Tim Bailey"
-            text3="SEO Specialist, Theme Junction"
+            srcr="/assets/images/client4.jpg"
+            text1="“Our online presence improved greatly after the redesign. Highly recommended!”"
+            text2="ALI RAZA"
+            text3="Founder of taekwondozone.pk"
           />
         </SwiperSlide>
         <SwiperSlide>
           <SwiperCard
             srcl="/assets/images/bd3897c84c7024bf14ef51e08d8e0451.png"
-            srcr="/assets/images/client1.png"
-            text1="“Taylor is a professional
-                     Designer he really helps my
-                     business by providing value to
-                      my business."
-            text2="Tim Bailey"
-            text3="SEO Specialist, Theme Junction"
+            srcr="/assets/images/client5.jpeg"
+            text1="“The professionalism and creativity Taylor brings to every project are unmatched. Our website and branding have never looked better!”"
+            text2="Noah Bennett"
+            text3="CEO, Urban Apparel"
           />
         </SwiperSlide>
         <SwiperSlide>
           <SwiperCard
             srcl="/assets/images/bd3897c84c7024bf14ef51e08d8e0451.png"
-            srcr="/assets/images/client1.png"
-            text1="“Taylor is a professional
-                     Designer he really helps my
-                     business by providing value to
-                      my business."
-            text2="Tim Bailey"
-            text3="SEO Specialist, Theme Junction"
+            srcr="/assets/images/client6.jpeg"
+            text1="“Working with Taylor was a great experience. His designs are clean, modern, and user-friendly.”"
+            text2="David Brown"
+            text3="Founder, Creative Minds"
           />
         </SwiperSlide>
       </Swiper>
-    </div >
+    </div>
   );
 }

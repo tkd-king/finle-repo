02f3a0fe29bf6243a30f741ac7d13632233f5fcd
tkdking00 <musama4288921@gmail.com>
2025-03-20 20:10:17 +0500

    floating btn

--- components/atoms/FloatingBtn.jsx
+++ components/atoms/FloatingBtn.jsx
@@ -4,7 +4,7 @@ const FloatingBtin = ({ onClick, icon, text }) => {
   return (
     <button
       onClick={onClick}
-      className="border-[1pt] border-white fixed bottom-5 right-5 bg-[#2A1454] text-white text-xl p-2 rounded-full shadow-lg hover:bg-[#2A1454]/90 transition-all flex items-center gap-2"
+      className="z-100 border-[1pt] border-white fixed bottom-5 right-5 bg-[#2A1454] text-white text-xl p-2 rounded-full shadow-lg hover:bg-[#2A1454]/90 transition-all flex items-center gap-2"
     >
       {icon && <span>{icon}</span>}
       {text && <span>{text}</span>}
